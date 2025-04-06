144180547f7f9e58f9940fe1baaa7f3abb5c591a
gvnd12 <govindshaju@gmail.com>
2025-04-06 23:26:44 +0530

    [ADD] Login user API [UPDATE] Code refactor

--- query/graph_query.py
+++ query/graph_query.py
@@ -1,5 +1,5 @@
-create_user_query ="""
-create(n:user{
+CREATE_USER_QUERY = """
+CREATE(n:user{
 name:$name,
 username:$username,
 password:$password,
@@ -7,4 +7,10 @@ mobile_no:$mobile_no,
 date_of_birth:$date_of_birth,
 gender:$gender,
 account_privacy:$account_privacy})
+RETURN n.username AS username
+"""
+
+LOGIN_USER_QUERY = """
+MATCH (n:user{username:$username,password:$password})
+RETURN n.username AS username,n.password AS password
 """
